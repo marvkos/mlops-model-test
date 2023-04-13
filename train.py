edef0eb8d1f77b0d6dfb19c3a6789f576d957f5f
marvkos <87067609+marvkos@users.noreply.github.com>
2023-04-13 17:54:12 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import sklearn
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
+import mlflow
+
 X, y = datasets.load_iris(return_X_y=True)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=1337)
@@ -9,4 +11,4 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_
 knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, Y_train)
 
-knn.score(X_test, y_test)
+mlflow.log_metric("accuracy", knn.score(X_test, y_test))
