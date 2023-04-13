5719a22d1d86ed43e226bb8121dc1c7472fdca69
marvkos <87067609+marvkos@users.noreply.github.com>
2023-04-13 17:46:42 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,5 +6,5 @@ X, y = datasets.load_iris(return_X_y=True)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=1337)
 
-knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=3)
+knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=4)
 knn.fit(X_train, Y_train)
