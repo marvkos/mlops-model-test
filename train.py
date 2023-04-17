c224326a80a2edc3980fe0f668fe6de3565544df
marvkos <87067609+marvkos@users.noreply.github.com>
2023-04-17 17:04:02 +0200

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,6 @@
 
 #knn.score(X_test, y_test)
 
-from argparse import ArgumentParser
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -86,30 +85,20 @@ def test(model, device, test_loader):
 
 def main():
     # Training settings
-    parser = ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    args = parser.parse_args()
+    args = {
+        "batch-size": 16,
+        "test-batch-size": 16,
+        "epochs": 14,
+        "lr": 1.0,
+        "gamma": 0.7,
+        "no-cuda": False,
+        "no-mps": False,
+        "dry-run": False,
+        "seed": 1,
+        "log-interval": 10,
+        "save-model": False
+    }
+    
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     use_mps = not args.no_mps and torch.backends.mps.is_available()
 
